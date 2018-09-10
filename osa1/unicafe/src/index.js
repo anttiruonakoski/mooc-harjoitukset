4ce6ccb88e19477f7b75236378b400b40bd0d9db
Antti Ruonakoski <aruonakoski@gmail.com>
2018-09-10 06:44:22 +0300

    exercises 1.9, 1.10 and 1.11

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -11,42 +11,40 @@ class App extends React.Component {
     }
   }
 
-  addGood = () => {
-    this.setState({
-      good: this.state.good + 1
-    })
+  addGeneric = (value) => {
+    return () => {
+    this.setState({ [value]: this.state[value] + 1 })
+    }
   }
 
-  addNeutral = () => {
-    this.setState({
-      neutral: this.state.neutral + 1
-    })
-  }
-
-  addBad = () => {
-    this.setState({
-      bad: this.state.bad + 1
-    })
-  }
 
   render() {
 
     const sumValues = obj => Object.values(obj).reduce((a, b) => a + b);
 
     const Statistics = ({ counters }) => {
+      if (counters.good > 0 || counters.neutral > 0 || counters.bad > 0) {
       return(
         <div>
-        <h3>statistiikka</h3>
+        <table>
         <Statistic quality='hyvä' counter={counters.good}/>
         <Statistic quality='neutraali' counter={counters.neutral}/>
         <Statistic quality='huono' counter={counters.bad}/>
         <Statistic quality='keskiarvo' counter={parseFloat((counters.good-counters.bad)/sumValues(counters)).toFixed(2)}/>
         <Statistic quality='positiivisia' counter={parseFloat((counters.good)/sumValues(counters)*100).toFixed(1) + " %"}/>
+        </table>
         </div>
         )
+      }
+      else {
+        return (
+          <div>ei yhtään palautetta annettu</div>
+        )
+      }
     }
 
-    const Statistic = ({ quality, counter }) => <div>{quality} {counter}</div>
+
+    const Statistic = ({ quality, counter }) => <tr><td>{quality}</td> <td>{counter}</td></tr>
 
     const Button = ({ handleClick, text }) => (
       <button onClick={handleClick}>
@@ -56,13 +54,14 @@ class App extends React.Component {
 
     return (
       <div>
-        <h3>anna palautetta</h3>
+        <h2>anna palautetta</h2>
         <div>
-          <Button text='hyvä' handleClick={this.addGood}/>
-          <Button text='neutraali' handleClick={this.addNeutral}/>
-          <Button text='huono' handleClick={this.addBad}/>
+          <Button text='hyvä' handleClick={this.addGeneric('good')}/>
+          <Button text='neutraali' handleClick={this.addGeneric('neutral')}/>
+          <Button text='huono' handleClick={this.addGeneric('bad')}/>
         </div>
         <div>
+        <h2>statistiikka</h2>
         <Statistics counters={this.state}/>
         </div>
       </div>
